bf2ce13ba15526e1c20e3d2f14081774af10c334
João Pedro Rodrigues <162360449+Jottynha@users.noreply.github.com>
2024-07-26 12:22:32 -0300

    joao-26/07/24

--- src/teste.cpp
+++ src/teste.cpp
@@ -4,35 +4,53 @@ int avaliarClasse(const unordered_map<tuple<int, int>, set<int>>& tabelaHash,
                   const unordered_map<int, set<int>>& tabelaHashClasses,
                   const vector<tuple<int, int>>& featuresLinha, int totalLinhas) {
     unordered_map<int, double> relevanciaClasse;
+    vector<set<int>> linhas;
+    for(int interacao=1;interacao<4;interacao++){
+        if(interacao==1){
+            for (const auto& tupla : featuresLinha) {
+            int coluna = get<0>(tupla);
+            int valor = get<1>(tupla);
+            set<int> linhasFeature = buscarFeature(tabelaHash, coluna, valor); 
+            linhas.push_back(linhasFeature);
+            for (const auto& classe : tabelaHashClasses) {
+                set<int> linhasClasse = classe.second;
+                set<int> linhasIntersecao;
+                set_intersection(linhasFeature.begin(), linhasFeature.end(),
+                                linhasClasse.begin(), linhasClasse.end(),
+                                inserter(linhasIntersecao, linhasIntersecao.begin()));
+                if (!linhasIntersecao.empty()) {
+                    double suporte = static_cast<double>(linhasIntersecao.size()) / totalLinhas;
+                    relevanciaClasse[classe.first] += suporte;
+                    }
+                }
+            }
+            vector<pair<int, double>> suporteClasses(relevanciaClasse.begin(), relevanciaClasse.end());
+            sort(suporteClasses.begin(), suporteClasses.end(), [](const pair<int, double>& a, const pair<int, double>& b) {
+                return a.second > b.second;
+            });
 
-    for (const auto& tupla : featuresLinha) {
-        int coluna = get<0>(tupla);
-        int valor = get<1>(tupla);
-        set<int> linhasFeature = buscarFeature(tabelaHash, coluna, valor);
-
-        for (const auto& classe : tabelaHashClasses) {
-            set<int> linhasClasse = classe.second;
-            set<int> linhasIntersecao;
-            set_intersection(linhasFeature.begin(), linhasFeature.end(),
-                             linhasClasse.begin(), linhasClasse.end(),
-                             inserter(linhasIntersecao, linhasIntersecao.begin()));
-
-            if (!linhasIntersecao.empty()) {
-                double suporte = static_cast<double>(linhasIntersecao.size()) / totalLinhas;
-                relevanciaClasse[classe.first] += suporte;
+            if (!suporteClasses.empty()) {
+                return suporteClasses.front().first;
+            } else {
+                return -1; // Nenhuma classe encontrada
             }
         }
-    }
-
-    vector<pair<int, double>> suporteClasses(relevanciaClasse.begin(), relevanciaClasse.end());
-    sort(suporteClasses.begin(), suporteClasses.end(), [](const pair<int, double>& a, const pair<int, double>& b) {
-        return a.second > b.second;
-    });
-
-    if (!suporteClasses.empty()) {
-        return suporteClasses.front().first;
-    } else {
-        return -1; // Nenhuma classe encontrada
+        else if(interacao==2){
+            for(int i=0;i<11;i++){
+                for(int j=i+1;j<11;j++){
+                set<int> intersecao;
+                set_intersection(linhas[i].begin(), linhas[j].end(),linhas[i].begin(), linhas[j].end(),inserter(intersecao, intersecao.begin()));
+                cout << "Interseccao: ";
+                for (const auto& linha : intersecao) {
+                            cout << linha << " ";
+                        }
+                cout << endl;
+                }
+            }
+        }
+        else {
+            return -1;
+        }
     }
 }
 
